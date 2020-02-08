9d922e9878b19418e8c62fbfce92d7df8ba4b1fd
albae <albae@10.242.3.85>
2020-02-08 16:51:07 -0500

    ready for review

---
+++ Alberici Project 1/src/project1/project1.java
@@ -0,0 +1,25 @@
+package project1;
+// Project 1
+// Author: Albe Alberici
+
+public class project1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Declaring variables
+		short age;
+		byte numberOfCats = 2;
+		
+		//modifying variables
+		age = 19;
+			
+		System.out.print("Hello there!\n ");
+		System.out.println("\tGeneral Kenobi!");
+		System.out.print("My age is " + age + ".");
+		System.out.println("");
+		System.out.println("I have " + numberOfCats + " cats.");
+	// Output above ^
+	}
+
+}
